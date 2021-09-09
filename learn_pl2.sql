97bf9cb4b11f2aa0b097b47a466f852f0322c2e6
Muhamad Novaldy Wiradharma <69621594+valdy24-code@users.noreply.github.com>
2021-09-09 20:26:50 +0700

    Add files via upload

---
+++ learn_pl2.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE learn_pl2
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 04, 2021 at 07:42 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `learn_pl2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `npm` INT(6) NOT NULL,
+  `nama` VARCHAR(50) NOT NULL
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`npm`, `nama`) VALUES
+(12, 'asas'),
+(110602, 'Saepudin'),
+(110608, 'Deni Pangale');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pbo_pegawai`
+--
+
+CREATE TABLE `pbo_pegawai` (
+  `nip` INT(8) NOT NULL,
+  `nama` VARCHAR(100) NOT NULL,
+  `jns_kel` CHAR(1) NOT NULL,
+  `tgl_lahir` DATE NOT NULL,
+  `status` CHAR(1) NOT NULL,
+  `mulai_kerja` DATE NOT NULL
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pbo_pegawai`
+--
+
+INSERT INTO `pbo_pegawai` (`nip`, `nama`, `jns_kel`, `tgl_lahir`, `status`, `mulai_kerja`) VALUES
+(132442, 'Dani Anggoro', 'L', '2021-09-08', 'M', '2021-09-15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`npm`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
